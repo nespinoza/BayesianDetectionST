f53b7652214b6973004bab2cabb86efcc68259c2
nespinoza <nespinoza@stsci.edu>
2020-10-11 18:29:55 -0400

    fitting code

---
+++ run_no_transit.py
@@ -0,0 +1,12 @@
+import juliet
+import numpy as np
+import glob
+
+folders = glob.glob('sim_*')
+for folder in folders:
+    files = glob.glob(folder+'/noisy_model*')
+    for lcfile in files:
+        print(lcfile)
+        f = lcfile.split('/')[-1].split('.dat')[0]
+        dataset = juliet.load(priors='priors_no_transit.dat', lcfilename=lcfile, out_folder = folder+'/'+f+'_results', verbose = True)
+        results = dataset.fit(use_dynesty=True, dynamic = True, dynesty_nthreads = 6, n_live_points = 1000)
